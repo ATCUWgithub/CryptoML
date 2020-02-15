d7e5f80e3da021109250879c80eb4c20e565f262
Abhishek Pratapa <abhishekpratapa@gmail.com>
2020-02-15 14:38:39 -0800

    Changed code to use Pytest

---
+++ infrastructure/crypto-ml/test/README.md
@@ -0,0 +1,3 @@
+# Testing
+
+Testing in this python package uses pytest. Install pytest and run it in this directory to run tests
--- infrastructure/crypto-ml/tests/test_example.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-    def test_isupper(self):
-        self.assertTrue(True)
-
-
-if __name__ == '__main__':
-    unittest.main()
